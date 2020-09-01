e4b7c56e09fda52031e9a7b2cd5f8a1938f3062c
BHanbashi <bhanbashi@gmail.com>
2020-09-01 20:51:59 +0300

    removed sleep command and jenkins creds query

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 kubectl apply -f jenkins.namespace.yaml -f jenkins.helm.yaml -f ingress.yaml
-WAIT=90
-echo "Sleeping for $WAIT"
-sleep $WAIT
-echo "Making progress"
-. query.sh
 kubectl create -n jenkins clusterrolebinding jenkins-account --clusterrole=cluster-admin --serviceaccount=jenkins:jenkins
